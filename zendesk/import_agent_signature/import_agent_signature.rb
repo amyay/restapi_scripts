dd2a67c1b1fb7314d0914beabe9937916bcdc768
amy <amy@zendesk.com>
2013-12-13 17:57:19 +0800

    created with the intention to help medidata import agent's new signature into their zendesk incompleted, but the rest api stuff appears to be working so far :)

---
+++ zendesk/import_agent_signature/import_agent_signature.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require './case.rb'
+require './user.rb'
+require './message.rb'
+require './string_extension.rb'
+require 'curb'
+require 'json'
+
+useremail = "amy@zendesk.com"
+
+c = Curl::Easy.new ("https://trial.zendesk.com/api/v2/users/search.json?query=%22"+useremail+"%22")
+c.http_auth_types = :basic
+c.username = "amy.tester.a001@gmail.com"
+c.password = "testmen0w"
+c.headers['Content-Type'] = "application/json"
+c.verbose = true
+c.http_get
+puts c.body_str
+
+userid = c.body_str.match(/{\"users\":\[\{\"id\":([^\/.]*),/)[1]
+
+# just to get rid of colour coding error on sublime"
+
+# puts "user id = " + userid
+
+targeturl = "https://trial.zendesk.com/api/v2/users/"+userid+".json"
+data = '{"user":{"signature":"script file signature 03\rthis is a new line\r\r\rthis is another line after some empty carriage return!"}}'
+
+c.url = targeturl
+c.http_put (data)
+
+
+
+# outfile_signature = File.open('./output/signature.txt', "wb")
+# outfile_signature << "curl -u amy.tester.a001@gmail.com:testmen0w -v -H \"Content-Type: application/json\" -X PUT https://trial.zendesk.com/api/v2/users/"+userid+".json -d'"
+# outfile_signature << "{ \"user\":{\"signature\":\"something\""
+# outfile_signature << "\n"
+
+# outfile_signature << "}}'"
+
+
+# Dump current User database
+# User.dump_storage
+# outfile_signature.close
