dc3ad37e18ce5bb2cf42dbf22fad3dc0a2dc3a86
amy <amy@zendesk.com>
2017-02-15 02:22:33 +0800

    - added SZ translation - remove unwanted comments - print DC ID with names at the end

--- zendesk/create_dynamic_content/Dynamiccontent.rb
+++ zendesk/create_dynamic_content/Dynamiccontent.rb
@@ -1,7 +1,7 @@
 class Dynamiccontent
-  attr_reader :name, :default_locale, :variant_1, :variant_26, :variant_47, :variant_9, :variant_81, :variant_77, :variant_1307
+  attr_reader :name, :default_locale, :variant_1, :variant_26, :variant_47, :variant_9, :variant_81, :variant_77, :variant_1307, :variant_10
 
-  def initialize name, default_locale, variant_1, variant_26, variant_47, variant_9, variant_81, variant_77, variant_1307
+  def initialize name, default_locale, variant_1, variant_26, variant_47, variant_9, variant_81, variant_77, variant_1307, variant_10
     @name = name
     @default_locale = default_locale
     @variant_1 = variant_1
@@ -11,6 +11,7 @@ class Dynamiccontent
     @variant_81= variant_81
     @variant_77 = variant_77
     @variant_1307 = variant_1307
+    @variant_10 = variant_10
   end
 end
 
@@ -22,4 +23,5 @@ end
 # 81 - th (Thai)
 # 77 - id (Indonesian)
 # 1307 - ms (Malay)
+# 10 - zh-CN (Simplified Chinese)
 
--- zendesk/create_dynamic_content/create_dynamic_content.rb
+++ zendesk/create_dynamic_content/create_dynamic_content.rb
@@ -7,7 +7,7 @@ require './Dynamiccontent.rb'
 require '../config.rb'
 
 # csv_filename = "my_dc.csv"
-csv_filename = "test.csv"
+# csv_filename = "test.csv"
 dc_array = Array.new
 created_dc_ids = Array.new
 data_default = nil
@@ -15,10 +15,13 @@ data_variants = nil
 count = 1
 error_count = 0
 current_dc_id = 0
+created_dc_name_id_hash = {}
+
+csv_filename = ARGV.shift
 
 # read input data
 CSV.foreach(csv_filename, :headers=>true) do |row|
-  dc = Dynamiccontent.new row["Title"],row["Default Locale"],row["Locale 1 Content"],row["Locale 26 Content"],row["Locale 47 Content"],row["Locale 9 Content"],row["Locale 81 Content"],row["Locale 77 Content"],row["Locale 1307 Content"]
+  dc = Dynamiccontent.new row["Title"],row["Default Locale"],row["Locale 1 Content"],row["Locale 26 Content"],row["Locale 47 Content"],row["Locale 9 Content"],row["Locale 81 Content"],row["Locale 77 Content"],row["Locale 1307 Content"], row["Locale 10 Content"]
   dc_array << dc
 end
 
@@ -65,8 +68,11 @@ dc_array.each do |dc|
     break
   else
     current_dc_id = results["item"]["id"]
+    current_dc_title = "#{dc.name}"
     created_dc_ids << current_dc_id
+    created_dc_name_id_hash[current_dc_title] = current_dc_id
   end
+
 
   # now create variants
 
@@ -76,7 +82,8 @@ dc_array.each do |dc|
   data_variants << "{\"locale_id\": 9, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_9}\"},"
   data_variants << "{\"locale_id\": 81, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_81}\"},"
   data_variants << "{\"locale_id\": 77, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_77}\"},"
-  data_variants << "{\"locale_id\": 1307, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_1307}\"}"
+  data_variants << "{\"locale_id\": 1307, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_1307}\"},"
+  data_variants << "{\"locale_id\": 10, \"active\": true, \"default\": false,\"content\": \"#{dc.variant_10}\"}"
   data_variants << "]}"
 
   puts "\nDEBUG: data:\n\n#{data_variants}\n\n"
@@ -102,127 +109,12 @@ dc_array.each do |dc|
 #   break if count == 3
 end
 
-puts "\n\n***************************************\n#{created_dc_ids.count} dynamic content CREATED : #{created_dc_ids.inspect}\n"
+puts "\n\n***************************************\n#{created_dc_ids.count} dynamic content CREATED\n"
+
+created_dc_name_id_hash.each do |title, id|
+  puts "#{id}        #{title}"
+end
 
 
 puts "\n\n***************************************\n#{error_count} ERRORS DETECTED - please check log for details\n\n" if error_count > 0
 
-
-
-
-# CSV.foreach(csv_filename, :headers=>true) do |row|
-#   # groups = row["Groups"].split(/[\s,]+/)
-#   groups = row["Groups"].split(/,\s|,/)
-#   if orig_agent_group_hash[row["Name"]] != nil
-#     puts "***ERROR -- Duplicate User '#{row["Name"]}' in CSV file"
-#     next
-#   end
-#   orig_agent_group_hash[row["Name"]] = groups
-# end
-
-
-
-# next_page = false
-# c = nil
-# # source_organizations = Array.new
-# # created_organization_ids = Array.new
-# error_count = 0
-# count = 1
-# data = nil
-
-
-# begin
-#   next_page = false
-#   # puts "******** count is #{count} *********"
-#   # puts "\n"
-
-#   c = Curl::Easy.new ("https://#{SOURCE_SUBDOMAIN}.zendesk.com/api/v2/organizations.json?page=#{count}")
-#   c.http_auth_types = :basic
-#   c.username = SOURCE_EMAIL
-#   c.password = SOURCE_PASSWORD
-#   c.headers['Content-Type'] = "application/json"
-#   c.verbose = true
-#   c.http_get
-#   # puts c.body_str
-
-#   # first, turns json into a hash
-#   results = JSON.parse (c.body_str)
-
-#   # now grab an array of tickets
-#   organization_list = results["organizations"]
-
-#   # within each item in ticket_list, it's a hash, so look for ticket IDs
-#   organization_list.each do |o|
-#     org = Organization.new o["name"], o["shared_tickets"], o["shared_comments"], o["external_id"], o["domain_names"], o["details"], o["notes"], o["group_id"], o["tags"], o["organization_fields"]
-#     source_organizations << org
-#   end
-
-#   # check to see if there are more pages to go
-#   next_page = !results["next_page"].nil?
-
-#   count += 1
-
-# end while next_page
-
-# # now prompts for user to copy all ticket fields
-# puts 'are you sure you are ready copy all organizations? (y/n)'
-# user_input = gets.chomp
-
-# if !(user_input.downcase == 'y' || user_input.downcase == 'yes')
-#   abort('abort copy of custom ticket fields by user!!')
-# end
-
-# count = 1
-
-
-# # set up copying
-# source_organizations.each do |o|
-
-# # puts "type = #{tf.type}, title = #{tf.title}"
-
-#   # skip importing system field
-#   # next if (!tf.system_field_options.nil?) || (ignore_types.include? tf.type)
-
-#   # set up data depending if it's dropdown or not
-#   data = "{\"organization\": {\"name\": \"#{o.name}\",\"shared_tickets\" : #{o.shared_tickets},\"shared_comments\": \"#{o.shared_comments}\",\"external_id\": #{o.external_id},\"domain_names\": #{o.domain_names},\"details\": #{o.details},\"notes\": #{o.notes},\"group_id\": #{o.group_id},\"tags\": #{o.tags},\"organization_fields\": {"
-
-#   o.organization_fields.each do |key,value|
-#     data << "\"#{key}\":"
-#     if value.nil?
-#       data << 'null,'
-#     elsif (value.kind_of? String)
-#       tempvalue = value.gsub("\n","\\n")
-#       data << "\"#{tempvalue}\","
-#     else
-#       data << "#{value},"
-#     end
-#   end
-
-#   data.chop!
-#   data << "}}}"
-
-# puts "\n\n#{data}\n\n"
-
-#   targeturl = "https://#{DESTINATION_SUBDOMAIN}.zendesk.com/api/v2/organizations.json"
-#   c.username = DESTINATION_EMAIL
-#   c.password = DESTINATION_PASSWORD
-#   c.url = targeturl
-#   c.http_post (data)
-#   results = JSON.parse (c.body_str)
-#   if !results["error"].nil?
-#     puts "ERROR: problems with adding organization"
-#     puts "Error description: #{results["error"]}"
-#     puts "Error details: #{results["message"]}\n"
-#     error_count += 1
-#   else
-#     created_organization_ids << results["organization"]["id"]
-#   end
-
-#   count += 1
-# #   break if count == 3
-# end
-
-# puts "\n\n***************************************\n#{created_organization_ids.count} organizations CREATED : #{created_organization_ids.inspect}\n"
-
-
-# puts "\n\n***************************************\n#{error_count} ERRORS DETECTED - please check log for details\n\n" if error_count > 0
