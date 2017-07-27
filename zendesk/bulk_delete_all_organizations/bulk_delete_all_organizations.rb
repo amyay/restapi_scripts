a29af4149fc9648b9fc312adcbd17928c472f13d
amy <amy@zendesk.com>
2017-07-28 01:25:59 +0800

    - updated "delete all orgniazation" script to include excluding orgnaizations (hardcoded array) - created "bulk delete all organizations"

--- zendesk/bulk_delete_all_organizations/bulk_delete_all_organizations.rb
+++ zendesk/bulk_delete_all_organizations/bulk_delete_all_organizations.rb
@@ -11,6 +11,7 @@ next_page = false
 c = nil
 organization_ids = Array.new
 count = 1
+ids_join = String.new
 
 # exclude organizations "accentgr" (11829852408) and "the iconic" (6075181187)
 excluded_organization_id = [11829852408, 6075181187]
@@ -58,6 +59,11 @@ else
   puts "organizations to be deleted: #{organization_ids.inspect}"
 end
 
+puts '****************************************************'
+puts "total number of organizations to be deleted: #{organization_ids.length}"
+puts '****************************************************'
+
+
 # now prompts for user to copy all ticket fields
 puts 'are you sure you are ready delete all organizations? (y/n)'
 user_input = gets.chomp
@@ -66,21 +72,44 @@ if !(user_input.downcase == 'y' || user_input.downcase == 'yes')
   abort('abort deletion of organizations by user!!')
 end
 
+# set up for deletion
+organization_ids.each_slice(100) do |section_ids|
+# convert array to string join by comma
+ids_join << section_ids.join(',')
 
-# iterate for deletion
-organization_ids.each do |id|
-  targeturl = "https://#{SUBDOMAIN}.zendesk.com/api/v2/organizations/#{id}.json"
-  c.url = targeturl
-  c.http_delete
-  if !c.body_str.lstrip.empty?
-    results = JSON.parse (c.body_str)
-    if !results["error"].nil?
-      puts "ERROR: cannot delete organization ID #{id}"
-      puts "Error description: #{results["description"]}"
-      puts "Error details: #{results["details"]["base"][0]["description"]}"
-      error_count += 1
-    end
-  end
+# puts ids_join
+
+targeturl = "https://#{SUBDOMAIN}.zendesk.com/api/v2/organizations/destroy_many.json?ids=#{ids_join}"
+c.url = targeturl
+c.http_delete
+results = JSON.parse (c.body_str)
+if !results["error"].nil?
+  puts "ERROR: problems with deletion of organizations"
+  puts "Error description: #{results["description"]}"
+  puts "Error details: #{results["details"]["base"][0]["description"]}"
+  error_count += 1
 end
+
+# reset ids_join
+ids_join = ""
+end
+
+
+
+# # iterate for deletion
+# organization_ids.each do |id|
+#   targeturl = "https://#{SUBDOMAIN}.zendesk.com/api/v2/organizations/#{id}.json"
+#   c.url = targeturl
+#   c.http_delete
+#   if !c.body_str.lstrip.empty?
+#     results = JSON.parse (c.body_str)
+#     if !results["error"].nil?
+#       puts "ERROR: cannot delete organization ID #{id}"
+#       puts "Error description: #{results["description"]}"
+#       puts "Error details: #{results["details"]["base"][0]["description"]}"
+#       error_count += 1
+#     end
+#   end
+# end
 
 puts "\n\n***************************************\n#{organization_ids.count} organizations DELETED : #{organization_ids.inspect}\n"
